e6d7688f824aa724456155f440be95f7043ad916
redfast00 <redfast00@gmail.com>
2020-05-20 20:34:05 +0200

    Add more transition_list functions

--- cli.pl
+++ cli.pl
@@ -13,7 +13,7 @@
 % testcases/basic.out is the output of the original Python assembler
 % This assembler gives the same output
 :- discontiguous main/1.
-% :- initialization(main, main).
+:- initialization(main, main).
 
 main([assemble, InFilename, OutFilename]) :-
   write("Starting to assemble\n"),
@@ -66,12 +66,48 @@ main([register_config, InFilename]) :-
   add_instructions(AsmList),
   write("Searching for input that results in a certain register configuration\n"),
   all_bytes(Asked),
-  register_state_transition_list([machine_state(0, 0, 0, 0, 0, [], virtual(Asked, []), init_array(0), 0, 0)|_], A, B, C, D),
-  write(Asked),
+  % TODO if there is time left, find a way to get the user to input these conditions instead of hardcoding them
+  %  this will take some time, since we'd have to parse user input
   A #= 2*B,
   A #> 1,
   C #= 5,
   D #= A + 1,
+  register_state_transition_list([machine_state(0, 0, 0, 0, 0, [], virtual(Asked, []), init_array(0), 0, 0)|_], A, B, C, D),
+  write(Asked),
+  write("\nEND OF PROGRAM,\ninput = "),
+  % Now there are still multiple possibilities, so label and get one of them
+  label(Asked),
+  write(Asked),nl.
+
+main([reach_ip, InFilename]) :-
+  ast_from_file(InFilename, AsmList),
+  write(AsmList),
+  add_instructions(AsmList),
+  write("Trying to reach certain IP\n"),
+  all_bytes(Asked),
+  % TODO take this from cli
+  ToReach #= 10,
+  Asked = [5],
+  reach_ip_transition_list([machine_state(0, 0, 0, 0, 0, [], virtual(Asked, []), init_array(0), 0, 0)|_], ToReach),
+  write(Asked),
+  write("\nEND OF PROGRAM,\ninput = "),
+  % Now there are still multiple possibilities, so label and get one of them
+  label(Asked),
+  write(Asked),nl.
+
+main([stack_config, InFilename]) :-
+  ast_from_file(InFilename, AsmList),
+  add_instructions(AsmList),
+  write("Searching for input that results in a certain stack configuration\n"),
+  all_bytes(Asked),
+  [First, Second, Third | _] = DesiredStack,
+  First #= Second,
+  Third #> 1,
+  stack_state_transition_list([machine_state(0, 0, 0, 0, 0, [], virtual(Asked, []), init_array(0), 0, 0)|_], DesiredStack),
+  write(Asked),
+  % TODO if there is time left, find a way to get the user to input these conditions instead of hardcoding them
+  %  this will take some time, since we'd have to parse user input
+
   write("\nEND OF PROGRAM,\ninput = "),
   % Now there are still multiple possibilities, so label and get one of them
   label(Asked),
--- transition.pl
+++ transition.pl
@@ -1,4 +1,4 @@
-:- module(transition, [emulate_transition_list/1, even_transition_list/1, register_state_transition_list/5]).
+:- module(transition, [emulate_transition_list/1, even_transition_list/1, register_state_transition_list/5, reach_ip_transition_list/2, stack_state_transition_list/2]).
 
 :- use_module(library(clpfd)).
 :- use_module(interpreter).
@@ -20,11 +20,30 @@ even_transition_list([Current, Next|T]) :- trans(Current, Next), even_transition
 
 % TODO check if we can somehow implicitly pass arguments around to avoid having to repeat the A, B, C, D arguments
 
+% transition_list for a certain register configuration that is passed as arguments. Accepts when machine is halted and
+%  register config is correct
 register_state_final_condition(machine_state(_IP, A, B, C, D, _Stack, _IO, _Memory, _Crashed, 1), A_c, B_c, C_c, D_c) :-
   A #= A_c,
   B #= B_c,
   C #= C_c,
   D #= D_c.
 
-  register_state_transition_list([Last], A, B, C, D)            :- register_state_final_condition(Last, A, B, C, D).
-  register_state_transition_list([Current, Next|T], A, B, C, D) :- trans(Current, Next), register_state_transition_list([Next|T], A, B, C, D).
+register_state_transition_list([Last], A, B, C, D)            :- register_state_final_condition(Last, A, B, C, D).
+register_state_transition_list([Current, Next|T], A, B, C, D) :- trans(Current, Next), register_state_transition_list([Next|T], A, B, C, D).
+
+% transition_list for when the machine reaches an instruction. Accepts when machine reaches the IP.
+
+reach_ip_final_condition(machine_state(IP, _A, _B, _C, _D, _Stack, _IO, _Memory, _Crashed, _Halted), IP_c) :-
+  IP #= IP_c.
+
+reach_ip_transition_list([Last], IP)            :- reach_ip_final_condition(Last, IP).
+reach_ip_transition_list([Current, Next|T], IP) :- trans(Current, Next), reach_ip_transition_list([Next|T], IP).
+
+% transition__list for a certain stack configuration that is passed as an argument. Accepts when machine is halted
+%  and stack config is correct
+
+stack_state_final_condition(machine_state(_IP, _A, _B, _C, _D, Stack, _IO, _Memory, _Crashed, 1), DesiredStack) :-
+  DesiredStack = Stack.
+
+stack_state_transition_list([Last], DesiredStack)            :- stack_state_final_condition(Last, DesiredStack).
+stack_state_transition_list([Current, Next|T], DesiredStack) :- trans(Current, Next), stack_state_transition_list([Next|T], DesiredStack).
