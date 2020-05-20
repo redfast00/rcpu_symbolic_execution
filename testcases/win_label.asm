da70cffdf18f8f2667e923805af41d5781b7082e
redfast00 <redfast00@gmail.com>
2020-05-20 20:14:04 +0200

    More content in win_label

--- testcases/win_label.asm
+++ testcases/win_label.asm
@@ -6,15 +6,28 @@ LDV B, 0
 PSH B
 SYS
 POP B
-LDV C, good:
-JLT B, C ;; jumps to C if A < B
-JMP bad:
-
-good:
+LDV C, win:
+;; jumps to C if A < B
+;; <=> jumps to win if A < B
+;; <=> jumps to win if 5 < B
+;; win if input > 5
+JLT B, C
+JMP fail:
 
 win:
-ADD C, D
+;; letter W (win)
+LDV A, 87
+PSH A
+LDV A, 1
+PSH A
+SYS
 HLT
 
-bad:
+fail:
+;; letter F
+LDV A, 0
+PSH A
+LDV A, 1
+PSH A
+SYS
 HLT
